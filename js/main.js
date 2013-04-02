20b4825910d4936a635dd2ed68be93a4cb77f994
oleg <dmitrymar@yahoo.com>
2013-04-02 18:56:42 -0400

    latest

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
 
+$("#sidenav-wrpr").hover(
+  function () {
+    $(".sidenav").removeClass("hidden");
+  },
+  function () {
+    $(".sidenav").addClass("hidden");
+  }
+  );
+
+});
